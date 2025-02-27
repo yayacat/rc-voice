8c58bb52f6f1b028204237657a6ff6c09e8389b3
yayacat <vcde0000@gmail.com>
2025-02-27 15:36:50 +0800

    fix: lint (#57)

--- src/components/modals/AddChannelModal.tsx
+++ src/components/modals/AddChannelModal.tsx
@@ -1,4 +1,4 @@
-/* eslint-disable @next/next/no-img-element */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React, { FormEvent, useState } from 'react';
 import { useSelector } from 'react-redux';
 
