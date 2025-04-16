005d64477a845bc2a5af5ec836ff7770d002ede9
Josh <joshhuang9508@gmail.com>
2025-04-16 22:04:34 +0800

    fix: 新增獲取成員創建時間的邏輯，並在修正過程中加入日誌輸出以便於調試

--- fix-member-create-at.js
+++ fix-member-create-at.js
@@ -10,12 +10,21 @@ const fixMemberCreateAt = async () => {
   for (const [key, member] of Object.entries(members)) {
     const { userId, serverId, id, ...rest } = member;
 
+    console.log(parseInt(rest.createdAt));
+
     await DB.set.member(userId, serverId, {
       createdAt: parseInt(rest.createdAt),
     });
+
+    console.log('GET: ', await getMemberCreateAt(userId, serverId));
   }
 
   console.log('member 修正完成');
+};
+
+const getMemberCreateAt = async (userId, serverId) => {
+  const member = await DB.get.member(userId, serverId);
+  return member.createdAt;
 };
 
 const main = async () => {
